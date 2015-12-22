1bcaa6ab012ff985ab2379f86300e02b6de651e6
Pesah Arthur <pesah@ensta.fr>
2015-12-23 00:27:43 +0100

    Add commentary to extractDetection.m (explain what it does)

--- extractDetection.m
+++ extractDetection.m
@@ -1,6 +1,11 @@
 function detection=showDetection(image, point, size_col, size_li)
+    % Cut a rectangle of size size_li*size_col, horizontally centered on the point
+    % and vertically cut with the point at 1/6 of the rectangle from the top 
+    % (to have all the traffic light, with the red fire at the top))
+    
     vertical_proportion = 1/6;
     image_size = size(image);
+    
     line = max(point(1)-floor(size_li*vertical_proportion),1);
     col = max(point(2)-floor(size_col/2),1);
     new_size = [size_li size_col];
