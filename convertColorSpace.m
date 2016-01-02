54a60103cf7c25233f22465e870e8c7d4ca29143
Arthur Pesah <arthur.pesah@ensta.fr>
2016-01-02 21:59:18 +0100

    Use the libraries in lib/ for convertColorSpace (to replace proprietary libraries)

--- convertColorSpace.m
+++ convertColorSpace.m
@@ -1,15 +1,16 @@
 function [lab_image, F] = convertColorSpace(image)
     % Conversion in lab
-    cform = makecform('srgb2lab');
-    lab_image = applycform(image,cform);
+    addpath('lib/RGB2Lab/')
+
+    lab_image = RGB2Lab(image);
 
     lab_image_normalized(:,:,1) = normalization(lab_image(:,:,1),0,256);
     lab_image_normalized(:,:,2:3) = normalization(lab_image(:,:,2:3),128,128);
-    
+
     % Getting F = L*(a+b)
     F = lab_image_normalized(:,:,1).*(lab_image_normalized(:,:,2)+lab_image_normalized(:,:,3));
     %imagesc(F);
-    
+
 % Question 3 : 3 dimensions dont les valeurs sont comprises entre 0 et
 % 255. Les deux premières dimensions sont les coordonnées de l'image et
 % la 3e représente la valeur de R, G ou B (3e dimension de taille 3)
