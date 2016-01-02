d77bff5015282e038432c5eba106ea14766cbef8
Arthur Pesah <arthur.pesah@ensta.fr>
2016-01-02 22:26:38 +0100

    Remove normalization in convertColorSpace (did not work properly)

--- convertColorSpace.m
+++ convertColorSpace.m
@@ -3,18 +3,10 @@ function [lab_image, F] = convertColorSpace(image)
     addpath('lib/RGB2Lab/')
 
     lab_image = RGB2Lab(image);
-
-    lab_image_normalized(:,:,1) = normalization(lab_image(:,:,1),0,256);
-    lab_image_normalized(:,:,2:3) = normalization(lab_image(:,:,2:3),128,128);
-
+    %lab_image_normalized(:,:,1) = normalization(lab_image(:,:,1));
+    %lab_image_normalized(:,:,2:3) = normalization(lab_image(:,:,2:3));
+    lab_image_normalized = lab_image;
     % Getting F = L*(a+b)
     F = lab_image_normalized(:,:,1).*(lab_image_normalized(:,:,2)+lab_image_normalized(:,:,3));
+    %display(F(1:50, 1:50));
     %imagesc(F);
-
-% Question 3 : 3 dimensions dont les valeurs sont comprises entre 0 et
-% 255. Les deux premières dimensions sont les coordonnées de l'image et
-% la 3e représente la valeur de R, G ou B (3e dimension de taille 3)
-
-% Question 4 : Toujours 3 dimensions et des valeurs entre 0 et 255.
-% Pour obtenir les bonnes plages de valeurs, il faut normaliser la
-% matrice.
